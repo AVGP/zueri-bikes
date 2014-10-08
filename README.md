a21aeeeba3832ff13f8050281eb418937cbce4ce
Martin N <mr.avgp@googlemail.com>
2014-10-08 16:18:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Züri Bikes
+
+## Warning: Work in progress!
+This is a working prototype that will eventually evolve in something that's usable.
+
+## What's this?
+This will be a handy little helper to find the nearest "Züri rollt" bike sharing station and the number of bikes available.
+
+Luckily Zürich makes this data available publicly and in real time (seriously, how cool is THAT?) so this is very, very helpful.
---
+++ app.js
@@ -0,0 +1,25 @@
+function parseBikeStationsFromXml(xml) {
+  var stations = [];
+  [].forEach.call(xml.childNodes[0].childNodes, function(bikeStation) {
+    stations.push({
+      name: bikeStation.querySelector("station_name"),
+      bikesAvailable: bikeStation.querySelector("nb_bikes")
+    })
+  }); 
+}
+
+function updateData(callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    callback(false, parseBikeStationsFromXml(this.responseXML));
+  };
+  xhr.overrideMimeType("text/xml");
+  xhr.open("get", "http://www.corsproxy.com/zh.suisseroule.ch/gestion/stats/rep_text.php?xml=1", true);
+  xhr.send();
+}
+
+function clearChildren(element) {
+  while(element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
